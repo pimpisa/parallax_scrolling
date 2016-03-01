f57b4a6cc506ae5dd4fbe113fc0554948ededc63
Pimpisa Predaswad <pimpisa.predaswad@gmail.com>
2016-03-01 14:50:17 -0500

    Final Commit

--- index.php
+++ index.php
@@ -6,8 +6,13 @@
 		<script type="text/javascript" src="js/jquery-1.12.1.min.js"></script>
 		<script type="text/javascript" src="js/jquery.cycle.all.js"></script>
 		<script type="text/javascript">
-			
-			
+			var ypos,image;
+			function parallax(){
+				ypos = window.pageYOffset;
+				image = document.getElementById('image');
+				image.style.top = ypos * .4 + 'px';
+			}
+			window.addEventListener('scroll',parallax)
 		</script>		
 		<style type="text/css">
 			*{
@@ -16,7 +21,7 @@
 			}
 			#image {
 				position:relative;
-				Z-index:-1;				
+				z-index:-1;				
 			}
 			img#image{
 				height:auto;
@@ -27,7 +32,7 @@
 				width:100%;
 				margin-top:-10px;
 				background-color:#4dbbac;
-				position:relativel
+				position:relative;
 				z-index:1;				
 			}
 		</style>
@@ -35,7 +40,7 @@
 	<body>
 		<img id="image" src="images/main_bg1.jpg">
 		<div id="content">
-			
+		    <img id="image" src="images/main_bg2.jpg">
 		</div>
 	</body>
 </html>
